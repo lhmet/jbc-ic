480114190b887a0180eaf6fdd970d67c791df9fe
lhmet <lhmet@ufsm.br>
2017-09-13 03:37:49 +0000

    required for extend series

--- R/complete_dates.R
+++ R/complete_dates.R
@@ -1,112 +1,9 @@
-
-time_step <- function(dates, verbose = TRUE) 
-{
- # dates <- x[["dates"]]
-  intervals <- dates %>%
-    diff() 
-
-  intervals_freq <- intervals %>%
-    table()
-  
-  deltat <- intervals_freq %>%
-    which.max() %>%
-    names() %>%
-    as.numeric()
-  
-  ## time unit
-  time_unit_str <- intervals %>%
-    min() %>%
-    capture.output() %>%
-    strsplit(" ") %>%
-    unlist()
-  
-  time_step_str <- paste(deltat, 
-                        time_unit_str[length(time_unit_str)],
-                        collapse = " ")
-  if (verbose) {
-    cat("Dataset time step:", time_step_str, "\n")
-    tab_interv <- intervals_freq %>%
-      prop.table() %>%
-      magrittr::multiply_by(100) %>%
-      round(2)
-    #tab <- round(prop.table(table(diff(X$date)))*100,2)
-    intervals_df <- tibble::data_frame(time = names(tab_interv),
-                                       freq = paste(tab_interv, "%"))
-    print(intervals_df)
-  }
-  return(time_step_str)
-}
-
-
-
-time_span <- function(dates, full_day = FALSE, tz = "UTC"){
-  # dates <- pga$date
-  tz(dates) <- "UTC"
-  date_min <- min(dates)
-  date_max <- max(dates)
-  
-  if (!full_day) {
-   return(tibble::data_frame(date_min, date_max))  
-  }
-  
-  ## obtain start and end date
-  m.min <- dates %>%
-    lubridate::minute() %>%
-    min() %>%
-    as.character() %>%
-    as.integer()
-  
-  m.min <- dates %>%
-    lubridate::minute() %>%
-    max() %>%
-    as.character() %>%
-    as.integer()
-  
-  h.min   <- dates %>%
-    lubridate::hour() %>%
-    min() %>%
-    as.character() %>%
-    as.integer()
-  
-  h.max   <- dates %>%
-    lubridate::hour() %>%
-    max() %>%
-    as.character() %>%
-    as.integer()
-  
-  sdate <- as.POSIXct(paste(as.Date(date_min)," ", 
-                            h.min,":",m.min, ":00",
-                            #0,":",30, ":00",
-                            sep = ""), 
-                      tz = tz)
-  edate <- as.POSIXct(paste(as.Date(date_max)," ",
-                            h.max,":",m.min, ":00",
-                            #0,":",0, ":00",
-                            sep = ""), 
-                      tz = tz)
-  return(tibble::data_frame(sdate, edate))
-}
-
-#time_span(pga$date)
-#time_step(pgaa$date)
-
-
-
 # Funçao para completar as datas ausentes no conjuno de dados
-complete_dates <- function(x, 
-                           group = "site",
-                           full_day = TRUE,
-                           verbose = FALSE,
-                           tz = "UTC"){
-  # x <- tar_data_sel; 
-  if (is.null(group)) {
-    x$site <- "my_site"
-    group <- "site"
-    group_was_null <- TRUE
-  }
+complete_dates <- function(x, group = "site", time_step = "hours"){
+  # x <- awsd
   xDT <- data.table::data.table(x, key = c("date", group))
   rm(x)
-  if (anyDuplicated(xDT) > 0) {
+  if(anyDuplicated(xDT) > 0){
     message(dup_rows, 
             " duplicated dates:", 
             " keeping the first data record.")
@@ -122,12 +19,11 @@ complete_dates <- function(x,
     t() %>% c()
   
   # time span
-  date_range <- time_span(dates = x[["date"]], full_day, tz)
-  
+  time_span <- range(x$date)
   # dates vector with constant and regular time step
-  all_dates <- seq(date_range[[1]], 
-                   date_range[[2]], 
-                   by = time_step(x[["date"]], verbose))
+  all_dates <- seq(time_span[1], 
+                   time_span[2], 
+                   by = time_step)
   
   # length(all_dates) * length(group_var)
   all_dates <- expand.grid(date = all_dates, 
@@ -135,22 +31,18 @@ complete_dates <- function(x,
   all_dates <- data.frame(all_dates) %>%
     dplyr::mutate(site = as.character(site))
   # nrow(all_dates)
-
-
-
-
+  
+  
   # join time series to match all_dates lenght  
   xc <- x %>%
     dtplyr::tbl_dt() %>%
     dplyr::right_join(dtplyr::tbl_dt(all_dates), 
                       by = c("date", group))
+  
   rm(x, all_dates)
   
-  if (group_was_null) {
-    xc$site <- NULL
-    rm(group)
-  }
   return(as_tibble(xc))
+  
 }
 
 
